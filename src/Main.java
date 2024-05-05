d40712e2d271619b9bb321caa9f263f0dab1d9dc
AlbertChoe <albert.choe73@gmail.com>
2024-05-05 21:03:16 +0700

    finishing CLI

--- src/Main.java
+++ src/Main.java
@@ -12,70 +12,58 @@ public class Main {
             return;
         }
 
-        System.out.print("Enter start word: ");
-        String startWord = scanner.nextLine().toLowerCase();
-        System.out.print("Enter end word: ");
-        String endWord = scanner.nextLine().toLowerCase();
+        String repeat = "y";
+        do {
+            System.out.print("Enter start word: ");
+            String startWord = scanner.nextLine().toLowerCase();
+            System.out.print("Enter end word: ");
+            String endWord = scanner.nextLine().toLowerCase();
 
-        if (!dictionary.contains(startWord) || !dictionary.contains(endWord)
-                || startWord.length() != endWord.length()) {
-            System.out.println("Invalid input words or word lengths are not equal.");
-            System.exit(0);
-        }
-        System.out.println(
-                "algorithm:\n 1.UCS (Uniform Cost Search)\n 2.GBFS (Greedy Best First Search)\n 3.A* (Astar) ");
-        System.out.print("Select algorithm : ");
-        String algorithm = scanner.nextLine().trim().toLowerCase();
+            if (!dictionary.contains(startWord) || !dictionary.contains(endWord)
+                    || startWord.length() != endWord.length()) {
+                System.out.println("Invalid input words or word lengths are not equal.");
+                continue;
+            }
+            System.out.println(
+                    "algorithm:\n 1.UCS (Uniform Cost Search)\n 2.GBFS (Greedy Best First Search)\n 3.A* (Astar) ");
+            System.out.print("Select algorithm : ");
+            String algorithm = scanner.nextLine().trim().toLowerCase();
 
-        long algStartTime = System.currentTimeMillis();
-        SearchResult path = new SearchResult(null, 0);
-        switch (algorithm) {
-            case "1":
-                path = UCS1.findPath(startWord, endWord, dictionary);
-                break;
-            case "2":
-                path = GBFS1.findPath(startWord, endWord, dictionary);
-                break;
-            case "3":
-                path = AStar.findPath(startWord, endWord, dictionary);
-                break;
-            default:
-                System.out.println("Invalid algorithm choice");
-                System.exit(0);
-        }
-        long algExecutionTime = System.currentTimeMillis() - algStartTime;
+            long algStartTime = System.currentTimeMillis();
+            SearchResult path = new SearchResult(null, 0);
+            switch (algorithm) {
+                case "1":
+                    path = UCS1.findPath(startWord, endWord, dictionary);
+                    break;
+                case "2":
+                    path = GBFS1.findPath(startWord, endWord, dictionary);
+                    break;
+                case "3":
+                    path = AStar.findPath(startWord, endWord, dictionary);
+                    break;
+                default:
+                    System.out.println("Invalid algorithm choice");
+                    continue;
+            }
+            long algExecutionTime = System.currentTimeMillis() - algStartTime;
 
-        if (path.getPath().isEmpty()) {
-            System.out.println("No path found between " + startWord + " and " + endWord);
-        } else {
-            List<String> pathResult = path.getPath().stream().map(String::toUpperCase).toList();
-            System.out.println("Path found: " + String.join(" -> ", path.getPath()));
-            System.out.println("Node visited: " + path.getVisitedCount());
-            System.out.println("Size: " + pathResult.size());
-        }
+            if (path.getPath() == null || path.getPath().isEmpty()) {
+                System.out.println("No path found between " + startWord + " and " + endWord);
+            } else {
+                List<String> pathResult = path.getPath().stream().map(String::toUpperCase).toList();
+                System.out.println("Path found: " + String.join(" -> ", path.getPath()));
+                System.out.println("Nodes visited: " + path.getVisitedCount());
+                System.out.println("Size: " + pathResult.size());
+            }
 
-        System.out.println("Execution time: " + algExecutionTime + " milliseconds");
+            System.out.println("Execution time: " + algExecutionTime + " milliseconds");
+            System.out.println(" ");
+            System.out.print("Do you want to try other words (y/n)? ");
+            repeat = scanner.nextLine().trim().toLowerCase();
+            System.out.println(" ");
+        } while (repeat.equals("y"));
+
+        System.out.println("Thanks ");
         scanner.close();
-
-        // SearchResult resultUCS = UCS1.findPath(startWord, endWord, dictionary);
-        // System.out.println("Path: " + resultUCS.getPath());
-        // System.out.println("Nodes visited: " + resultUCS.getVisitedCount() + "\nsize
-        // : " + resultUCS.getPath().size());
-
-        // SearchResult resultGBFS = GBFS1.findPath(startWord, endWord, dictionary);
-        // System.out.println("Path (Greedy Best-First Search): " +
-        // resultGBFS.getPath());
-        // System.out
-        // .println("Nodes visited: " + resultGBFS.getVisitedCount() + "\nsize : " +
-        // resultGBFS.getPath().size());
-
-        // SearchResult resultAStar = AStar.findPath(startWord, endWord, dictionary);
-        // System.out.println("Path (A* Search): " + resultAStar.getPath());
-        // System.out
-        // .println(
-        // "Nodes visited: " + resultAStar.getVisitedCount() + "\nsize : " +
-        // resultAStar.getPath().size());
-
-        // scanner.close();
     }
 }
